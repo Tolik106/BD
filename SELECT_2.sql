c0bdccc83cddfef034fbc160e601be8ce80d4065
Анатолий Савин <beningtonchess@yandex.ru>
2023-07-10 20:15:37 +0300

    home work __

--- SELECT_2.sql
+++ SELECT_2.sql
@@ -13,15 +13,14 @@ join album on track.album_id  = album.id
 group by album_name ;
 
 
-SELECT music_artist_name /* Получаем имена исполнителей */
-FROM music_artist  /* Из таблицы исполнителей */
-WHERE music_artist_name NOT IN ( /* Где имя исполнителя не входит в вложенную выборку */
-    SELECT music_artist_name /* Получаем имена исполнителей */
-    FROM artist /* Из таблицы исполнителей */
-    JOIN album_artist on music_artist.id  = album_artist.music_artist_id /* Объединяем с промежуточной таблицей */
-    JOIN join album on album.id = album_artist.album_id/* Объединяем с таблицей альбомов */
-    WHERE year_of_relese = 2020 /* Где год альбома равен 2020 */
-
+SELECT music_artist_name 
+FROM music_artist  
+WHERE music_artist_name NOT IN (
+    SELECT music_artist_name 
+    FROM music_artist 
+    JOIN album_artist on music_artist.id  = album_artist.music_artist_id 
+    JOIN album on album.id = album_artist.album_id
+    WHERE year_of_release = 2020);
 
 select collection_name from collection 
 join collection_track on collection.id = collection_track.collection_id 
